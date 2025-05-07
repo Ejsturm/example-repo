99cdf901b5db85bdae20f02d3bf8510def9dc73c
Erica S <Sturm.ericaj@gmail.com>
2025-05-07 15:38:12 -0500

    Initial commit
    This is the first submission for the autograded task.

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/Code Files/hello_world.py
@@ -0,0 +1,20 @@
+"""
+The python program below will ask the user for some information and then print
+out a formatted version of it. 2025-05-07 EJS
+"""
+
+""" Pseudocode
+Ask user for their name, store as variable user_name
+Print user_name
+Ask user for their age, store as variable user_age
+Print user_age
+Print "Hello, World!"
+"""
+
+user_name = input("What is your name? ")
+print(f"The user's name is {user_name}.")
+
+user_age = input("What is your age? ")
+print(f"The user's age is {user_age}.")
+
+print("Hello, World!")
