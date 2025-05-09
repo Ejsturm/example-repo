ebe8575500a1525a8230d6b6b0d348ce7d16f8b5
Erica S <Sturm.ericaj@gmail.com>
2025-05-08 23:08:06 -0500

    Initial commit
    Creating basic lists and printing their info.

---
+++ Level 1 - Python for Software Engineering/M02T01 – Data Structures – The List/Code Files/list_types.py
@@ -0,0 +1,14 @@
+""" Working with lists: creating them, storing information in them, 
+and accessing info within them. 2025-05-08 EJS
+"""
+friends_names = ["Alice", "Bob", "Charlie"]
+print(friends_names[0])
+print(friends_names[-1])
+print(len(friends_names))
+
+friends_ages = [3, 1, 4]
+
+# My kingdom for a for loop.
+print(f"{friends_names[0]} is {friends_ages[0]} years old.")
+print(f"{friends_names[1]} is {friends_ages[1]} years old.")
+print(f"{friends_names[2]} is {friends_ages[2]} years old.")
